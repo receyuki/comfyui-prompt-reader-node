f4b43504dc50a6cfb9e943b71d8157f9b4daec9f
receyuki <receyuki@gmail.com>
2023-09-14 02:24:35 +0800

    Add nodes.py

---
+++ nodes.py
@@ -0,0 +1,11 @@
+"""
+@author: receyuki
+@title: SD Prompt Reader
+@nickname: SD Prompt Reader
+@description: ComfyUI node version of SD Prompt Reader
+"""
+
+from stable_diffusion_prompt_reader.sd_prompt_reader.image_data_reader import (
+    ImageDataReader,
+)
+from stable_diffusion_prompt_reader.sd_prompt_reader.__version__ import VERSION
